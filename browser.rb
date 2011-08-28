609afba4dad134cb2636c0b4cdeb5fbeccddcc17
spiralofhope <spiralofhope@lavabit.com>
2011-08-27 22:26:35 -0700

    Style rewrite.

--- browser.rb
+++ browser.rb
@@ -1,21 +1,29 @@
-Shoes.app :title => "shoes-contrib browser", :height => 75, :width => 355 do
+# This ignores dot directories.
+files = Dir.glob( '*' )
+
+Shoes.app(
+            :title => "shoes-contrib browser",
+            :height => 75,
+            :width => 355
+          ) do
   stack do
     flow do
-      para "Choose a category:"
-      @category_box = list_box :items => %w[animation app art basic elements events expert good kernel manipulation position simple styles]
+      para 'Choose a category:'
+      @category_box = list_box( :items => files )
     end
-    @example_flow = flow :hidden => true do
-      para "Choose an example:"
-      @example_box = list_box :items => []
+    
+    @example_flow = flow( :hidden => true ) do
+      para 'Choose an example:'
+      @example_box = list_box( :items => [] )
     end
-
+    
     @category_box.change do |box|
-      @example_flow.style :hidden => false
-      @example_box.items = Dir.glob("#{box.text}/*.rb")
+      @example_flow.style( :hidden => false )
+      @example_box.items = Dir.glob( "#{box.text}/*.rb" )
     end
-
+    
     @example_box.change do |box|
-      eval(File.open(box.text, "rb").read, TOPLEVEL_BINDING)
+      eval( File.open( box.text, 'rb' ).read, TOPLEVEL_BINDING )
     end
   end
 end
