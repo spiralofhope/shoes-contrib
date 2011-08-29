6b4dc54734aad806d6ae3c6747ff25aac866cf17
spiralofhope <spiralofhope@lavabit.com>
2011-08-29 09:40:46 -0700

    Style changes

--- simple/bounce.rb
+++ simple/bounce.rb
@@ -1,22 +1,31 @@
-xspeed, yspeed = 8.4, 6.6
-xdir, ydir = 1, 1
+xspeed = 8.4
+yspeed = 6.6
+xdir = 1
+ydir = 1
 
 Shoes.app do
   background "#DFA"
-  border black, :strokewidth => 6
-
+  border(
+    black,
+    :strokewidth => 6
+  )
   nostroke
-  @icon = image "#{DIR}/static/shoes-icon.png", :left => 100, :top => 100 do
+  @icon = image(
+      "#{DIR}/static/shoes-icon.png",
+      :left => 100,
+      :top => 100
+    ) do
     alert "You're soooo quick."
   end
 
-  x, y = self.width / 2, self.height / 2
+  x = self.width / 2
+  y = self.height / 2
   size = @icon.size
-  animate(30) do
-    x += xspeed * xdir
-    y += yspeed * ydir
+  animate( 30 ) do
+    x = x + xspeed * xdir
+    y = y + yspeed * ydir
     
-    xdir *= -1 if x > self.width - size[0] or x < 0
+    xdir *= -1 if x > self.width  - size[0] or x < 0
     ydir *= -1 if y > self.height - size[1] or y < 0
 
     @icon.move x.to_i, y.to_i
