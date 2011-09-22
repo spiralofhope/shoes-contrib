c396e665d348ca679020d64b7e594549d0a31d4e
spiralofhope <spiralofhope@lavabit.com>
2011-09-22 08:37:26 +0000

    Bug fix - programs are viewable again.  Simplified that code.

--- browser.rb
+++ browser.rb
@@ -27,7 +27,34 @@ require 'lib-browser.rb'
 
 #
 # 0 - The list of categories.
+# @content
+# @search
+# @search_button
 def main()
+  #
+  def view_search( string )
+    @content.clear
+    # A basic limitation since a one or two-character search is too aggressive and not actually useful.
+    return '' if string.length < 3
+    view_search_hash = Hash.new
+    @@tags_hash.each_key{ |k|
+      view_search_hash["#{ k }"] = Array.new
+      @@tags_hash[k].each{ |v|
+        view_search_hash["#{ k }"] << v if /#{ string }/ =~ v
+      }
+    }
+    view_search_hash.delete_if{ |k,v|
+      view_search_hash[k] == []
+    }
+    view_search_hash.each_key{ |k|
+      s = ''
+      view_search_hash[k].each{ |v|
+        s << v
+      }
+      view_program_summary( k, s )
+    }
+  end # view_search( string )
+  #
   # TODO:  Can I  have the cursor automatically placed in the edit_line when the program launches?  Maybe toss a tab character at the keyboard?
   # @self.width doesn't understand a scroll bar!  TODO:  How can I know if a scroll bar is being painted or not?  How do I know the size of a scroll bar?
   @search = (
@@ -39,7 +66,6 @@ def main()
   )
   #
   @content = flow{}
-  #view_all()
   view_categories_list()
   #
   @search.change do |s|
@@ -53,39 +79,92 @@ def main()
   #
 end # main()
 
+def view_program_summary( directory, *splat )
+  if splat != [] then
+    tags = splat[0]
+    tags = " (#{ tags.to_s })"
+    tags = '' if directory == splat[0]
+  end
+  @content.append do
+    flow( :margin_top => 10 ) do
+      background( lightyellow, :curve => 10, :margin_left => 5, :margin_right => 20 )
+      stack( width: 150 ) do
+        #para  # Blank line above the thumbnail.
+        program_thumbnail( directory )
+      end
+      flow( width: width-150 ) do
+        para( link( directory ){ view_a_program( directory ) }, tags, "\n" )
+        # TODO:  How would I have this text be on the same line as the program name (the line above this one) and be right-aligned?
+        button "Run" do
+          program_run( directory )
+        end
+        button "Edit" do
+          editor( directory )
+        end
+        # FIXME:  This text needs a right margin.  I can't find the documentation for that.  (RE-TEST)
+        para( program_description( directory ) )
+      end
+    end
+  end
+end # view_program_summary( directory, *splat )
+
 #
 # 1 - This is the first thing that the user sees.
+# @category
+# @content
 def view_categories_list()
+
+  def view_category_summary( category_name )
+    #
+    # This is largely cloned from program_thumbnail()
+    def catgegory_thumbnail( category_name )
+      dir = File.join( '..', 'categories' )
+      i = File.join( '..', 'categories', 'default-thumbnail.png' )
+      f = File.join( dir, "#{ category_name }.png" )
+      i = f if File.exists?( f )
+      f = File.join( dir, "#{ category_name }.jpg" )
+      i = f if File.exists?( f )
+      image(
+        i,
+        width: 150,
+        height: 150,
+        :margin_left => 10,
+        :margin_bottom => 5,
+        :margin_top => 5
+      ).click{ view_a_category( category_name ) }
+    end # catgegory_thumbnail( category_name )
+    #
+    @category = ''
+    @content.append do
+      flow( :margin_top => 10 ) do
+        background( lightyellow, :curve => 10, :margin_left => 5, :margin_right => 20 )
+        stack( width: 150 ) do
+          #para  # Blank line above the thumbnail.
+          catgegory_thumbnail( category_name )
+        end
+        flow( width: width-150 ) do
+          para( link( category_name ){ view_a_category( category_name ) } )
+        end
+      end
+    end
+  end # view_category_summary( category_name, *splat )
+
   @content.clear
   @@categories_array.each{ |c|
     view_category_summary( c )
   }
 end # view_categories_list()
 
-def view_category_summary( category_name )
-  @category = ''
-  @content.append do
-    flow( :margin_top => 10 ) do
-      background( lightyellow, :curve => 10, :margin_left => 5, :margin_right => 20 )
-      stack( width: 150 ) do
-        #para  # Blank line above the thumbnail.
-        catgegory_thumbnail( category_name )
-      end
-      flow( width: width-150 ) do
-        para( link( category_name ){ view_a_category( category_name ) } )
-      end
-    end
-  end
-end # view_category_summary( category_name, *splat )
-
-# This is largely cloned from program_thumbnail(), above.
-def catgegory_thumbnail( category_name )
-  dir = File.join( '..', 'categories' )
-  i = File.join( '..', 'categories', 'default-thumbnail.png' )
-  f = File.join( dir, "#{ category_name }.png" )
+def program_thumbnail( directory )
+  # TODO:  This is a cumbersome way to do this.
+  # TODO:  What image file types does Shoes support?
+  # FIXME/TODO:  How would I get an animated image to appear?  I don't even know how to properly take those.  Do I have to manually animate the image by swapping out multiple images?  Eww!
+  i = File.join( 'default-thumbnail.png' )
+  f = File.join( directory, "thumbnail.png" )
   i = f if File.exists?( f )
-  f = File.join( dir, "#{ category_name }.jpg" )
+  f = File.join( directory, "thumbnail.jpg" )
   i = f if File.exists?( f )
+  # If the default thumbnail doesn't actually exist, then this would gracefully default to painting an empty space of the appropriate size (150 x 150px, margins, etc).
   image(
     i,
     width: 150,
@@ -93,35 +172,12 @@ def catgegory_thumbnail( category_name )
     :margin_left => 10,
     :margin_bottom => 5,
     :margin_top => 5
-  ).click{ view_a_category( category_name ) }
-end # catgegory_thumbnail( category_name )
+  ).click{ view_a_program( directory ) }
+end # program_thumbnail( directory )
 
 def program_run( directory )
   eval( File.open( File.join( directory, "#{ directory }.rb" ) ).read, TOPLEVEL_BINDING )
 end # program_run( directory )
-
-def view_search( string )
-  @content.clear
-  # A basic limitation since a one or two-character search is too aggressive and not actually useful.
-  return '' if string.length < 3
-  view_search_hash = Hash.new
-  @@tags_hash.each_key{ |k|
-    view_search_hash["#{ k }"] = Array.new
-    @@tags_hash[k].each{ |v|
-      view_search_hash["#{ k }"] << v if /#{ string }/ =~ v
-    }
-  }
-  view_search_hash.delete_if{ |k,v|
-    view_search_hash[k] == []
-  }
-  view_search_hash.each_key{ |k|
-    s = ''
-    view_search_hash[k].each{ |v|
-      s << v
-    }
-    view_program_summary( k, s )
-  }
-end # view_search( string )
 
 #
 # 2 - Clicking on a category
@@ -145,26 +201,6 @@ def view_a_category( category_name )
   }
 end # view_a_category( category_name )
 
-def program_thumbnail( directory )
-  # TODO:  This is a cumbersome way to do this.
-  # TODO:  What image file types does Shoes support?
-  # FIXME/TODO:  How would I get an animated image to appear?  I don't even know how to properly take those.  Do I have to manually animate the image by swapping out multiple images?  Eww!
-  i = File.join( 'default-thumbnail.png' )
-  f = File.join( directory, "thumbnail.png" )
-  i = f if File.exists?( f )
-  f = File.join( directory, "thumbnail.jpg" )
-  i = f if File.exists?( f )
-  # If the default thumbnail doesn't actually exist, then this would gracefully default to painting an empty space of the appropriate size (150 x 150px, margins, etc).
-  image(
-    i,
-    width: 150,
-    height: 150,
-    :margin_left => 10,
-    :margin_bottom => 5,
-    :margin_top => 5
-  ).click{ view_program( directory ) }
-end # program_thumbnail( directory )
-
 def back_to_main()
   @content.remove
   @search.remove
@@ -172,36 +208,37 @@ def back_to_main()
   main()
 end # back_to_main()
 
+def program_description( program_directory )
+  # Just kludging it for now.
+  # TODO:  I should use some recognized format for descriptions.  Intelligently parse the file and pull it out.  Maybe even look for a README, file_id.diz, descript.ion or some such?
+  # TODO:  Automatically link() hyperlinks.
+  # TODO:  Automatically markup text markup.
+  # TODO:  If I change the description, then add a button to view the unaltered description.
+  return 'aaaaa ' * 20
+end # program_description( program_directory )
+
 #
 # 3 - Clicking on a program
-def view_program_summary( directory, *splat )
-  if splat != [] then
-    tags = splat[0]
-    tags = " (#{ tags.to_s })"
-    tags = '' if directory == splat[0]
-  end
-  @content.append do
-    flow( :margin_top => 10 ) do
-      background( lightyellow, :curve => 10, :margin_left => 5, :margin_right => 20 )
-      stack( width: 150 ) do
-        #para  # Blank line above the thumbnail.
-        program_thumbnail( directory )
-      end
-      flow( width: width-150 ) do
-        para( link( directory ){ view_program( directory ) }, tags, "\n" )
-        # TODO:  How would I have this text be on the same line as the program name (the line above this one) and be right-aligned?
-        button "Run" do
-          program_run( directory )
-        end
-        button "Edit" do
-          editor( directory )
-        end
-        # FIXME:  This text needs a right margin.  I can't find the documentation for that.  (RE-TEST)
-        para( program_description( directory ) )
-      end
-    end
-  end
-end # view_program_summary( directory, *splat )
+def view_a_program( directory )
+  @search.remove
+  @search_button.remove
+  @content.remove
+  @content = stack{
+   flow( :margin_left => 10 ){
+      para(
+        link( 'Back' ){ back_to_main() },
+        strong( " #{ directory }" )
+      )
+    }
+  }
+  # Appends the program summary.
+  view_program_summary( directory )
+  @content.append{
+    stack( :margin_left => 10, :margin_top => 5, :margin_right => 20, :margin_top => 5 ){
+      para( program_contents( directory ) )
+    }
+  }
+end # view_a_program( directory )
 
 # TODO:  Syntax highlighting.  Somehow.
 def program_contents( program_directory )
@@ -215,35 +252,6 @@ def program_contents( program_directory )
   f.close
   return string
 end # program_contents( program_directory )
-
-def program_description( program_directory )
-  # Just kludging it for now.
-  # TODO:  I should use some recognized format for descriptions.  Intelligently parse the file and pull it out.  Maybe even look for a README, file_id.diz, descript.ion or some such?
-  # TODO:  Automatically link() hyperlinks.
-  # TODO:  Automatically markup text markup.
-  # TODO:  If I change the description, then add a button to view the unaltered description.
-  return 'aaaaa ' * 20
-end # program_description( program_directory )
-
-def view_program( directory )
-  @search.remove
-  @search_button.remove
-  @content.remove
-  @content = stack{
-    background( lightyellow, :curve => 10, :margin_left => 5, :margin_right => 20, :margin_top => 5 )
-    stack( :margin_left => 10, :margin_top => 5, :margin_right => 20, :margin_top => 5 ){
-      para( back_to_main() )
-      title( directory )
-      button "Run" do
-        program_run( directory )
-      end
-    }
-    stack( :margin_left => 20 ){
-    program_thumbnail( directory )
-    para( program_contents( directory ) )
-    }
-  }
-end # view_program( directory )
 
 Shoes.app(
             :title => "Program Browser",
